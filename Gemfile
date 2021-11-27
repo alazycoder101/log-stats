bf9c83866fc69cec9b5ae1c23f7b8c4bcb13bf34
lazycoder <alazycoder101@gmail.com>
2021-11-27 13:45:29 +0000

    rubocop and simplecov added

--- Gemfile
+++ Gemfile
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 source 'http://rubygems.org'
 
 group :development do
   gem 'rspec', '~>3.1'
+  gem 'rubocop'
+end
+
+group :test do
+  gem 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
-# encoding: UTF-8
-$:.push File.expand_path("../lib", __FILE__)
+# frozen_string_literal: true
+
+if ENV.fetch('COVERAGE', false)
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+    minimum_coverage 90
+    maximum_coverage_drop 2
+  end
+end
+
+$LOAD_PATH.push File.expand_path('lib', __dir__)
 require 'cli'
 
 RSpec.configure do |c|
-
   c.after(:suite) do
   end
 end
