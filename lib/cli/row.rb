21b92e1aa023d68e836b80ceaed38f197d6e9701
Lazy coder <82757613+alazycoder101@users.noreply.github.com>
2024-09-01 17:54:21 +0100

    row to mapped to result

--- lib/cli/row.rb
+++ lib/cli/row.rb
@@ -3,10 +3,30 @@
 module CLI
   # Row: row in log file
   class Row
-    attr_accessor :fields
+    HEADERS = %i[path ip].freeze
+    SEPERATOR = ' '
 
-    def initialize(*fields)
-      @fields = fields.first.is_a?(Array) ? fields.first : fields
+    attr_reader :path, :ip, :fields
+
+    def initialize(text)
+      parse(text)
+      split
+    end
+
+    private
+
+    def parse(text)
+      if text.is_a? String
+        @fields = text.split(SEPERATOR)
+      elsif text.is_a? Array
+        @fields = text
+      end
+    end
+
+    def split
+      HEADERS.each_with_index do |header, index|
+        instance_variable_set :"@#{header}", fields[index]
+      end
     end
   end
 end
--- spec/lib/cli/row_spec.rb
+++ spec/lib/cli/row_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 describe CLI::Row do
   describe 'initialize' do
     it 'should create a new Row object' do
-      row = CLI::Row.new
+      row = CLI::Row.new('a b')
       expect(row).to be_a CLI::Row
     end
 
     it 'should create with fields' do
-      row = CLI::Row.new(1, 2)
-      expect(row.fields).to eql([1, 2])
+      row = CLI::Row.new('/a/b 192.168.0.1')
+      expect(row.fields).to eql(['/a/b', '192.168.0.1'])
     end
 
     it 'should create with fields when passing array' do
