bb5d5191124a3c99571b3be16b9521fe8d23a4f8
Lazy coder <82757613+alazycoder101@users.noreply.github.com>
2024-09-01 18:02:45 +0100

    change the way of counting to save more memory

--- lib/cli/counter.rb
+++ lib/cli/counter.rb
@@ -5,34 +5,13 @@ module CLI
   class Counter
     attr_reader :stats
 
-    def self.count(rows)
-      counter = Counter.new(rows)
-      counter.count
+    def initialize
+      @stats = {}
     end
 
-    def self.unique_count(rows)
-      counter = Counter.new(rows)
-      counter.unique
-    end
-
-    def initialize(rows = [])
-      @rows = rows
-    end
-
-    def count
-      group.map { |variable| [variable[0], variable[1].size] }
-    end
-
-    def group
-      @group ||= @rows.group_by { |row| row.fields[0] }
-    end
-
-    def unique
-      group.map do |variable|
-        [variable[0], variable[1].map do |row|
-                        row.fields[1]
-                      end.uniq.size]
-      end
+    def count(row)
+      @stats[row.path] ||= 0
+      @stats[row.path] += 1
     end
   end
 end
--- spec/lib/cli/counter_spec.rb
+++ spec/lib/cli/counter_spec.rb
@@ -2,27 +2,34 @@
 
 require 'spec_helper'
 describe CLI::Counter do
+  subject(:counter) { CLI::Counter.new }
+
   let(:rows) do
     [
-      CLI::Row.new('/help_page/1', '126.318.035.038'),
-      CLI::Row.new('/contact', '184.123.665.067'),
-      CLI::Row.new('/home', '184.123.665.067'),
-      CLI::Row.new('/about/2', '444.701.448.104'),
-      CLI::Row.new('/help_page/1', '929.398.951.889'),
-      CLI::Row.new('/help_page/1', '126.318.035.038'),
-      CLI::Row.new('/help_page/1', '722.247.931.582')
+      CLI::Row.new('/help_page/1 126.318.035.038'),
+      CLI::Row.new('/contact 184.123.665.067'),
+      CLI::Row.new('/home 184.123.665.067'),
+      CLI::Row.new('/about/2 444.701.448.104'),
+      CLI::Row.new('/help_page/1 929.398.951.889'),
+      CLI::Row.new('/help_page/1 126.318.035.038'),
+      CLI::Row.new('/help_page/1 722.247.931.582')
     ]
   end
 
-  describe '#count' do
-    it 'should return visits' do
-      result = CLI::Counter.count(rows)
-      expect(result[0]).to eq(['/help_page/1', 4])
+  describe '.new' do
+    it 'should create a new Counter object' do
+      expect(counter).to be_a CLI::Counter
     end
 
-    it 'should return unique visits' do
-      result = CLI::Counter.unique_count(rows)
-      expect(result[0]).to eq(['/help_page/1', 3])
+    it 'should create a empty stats' do
+      expect(counter.stats).to eq({})
+    end
+  end
+
+  describe '#count' do
+    it 'increments visits' do
+      expect { counter.count(rows[0]) }.to change { counter.stats }.from({}).to('/help_page/1' => 1)
+      expect { counter.count(rows[0]) }.to change { counter.stats }.to('/help_page/1' => 2)
     end
   end
 end
